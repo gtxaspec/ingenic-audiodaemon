da78e4637731c7d6d1f36553394bbdbf4f415678
gtxaspec <gtxent@gmail.com>
2023-09-28 08:54:24 -0700

    web_client code modifications

--- src/web_client/web_client.c
+++ src/web_client/web_client.c
@@ -1,77 +1,74 @@
-/* INGENIC AUDIO CLIENT */
+/* INGENIC WEBSOCKET AUDIO CLIENT */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <libwebsockets.h>
 #include "web_client/cmdline.h"
 #include "web_client/client_network.h"
 #include "web_client/playback.h"
 #include "version.h"
 
+static int active_ws_connections = 0;  // Number of active WebSocket connections
+static int daemon_sockfd = -1;  // Socket file descriptor for daemon connection
+
+// WebSocket callback
+static int ws_callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+    switch (reason) {
+        case LWS_CALLBACK_ESTABLISHED:
+            active_ws_connections++;
+            if (daemon_sockfd == -1) {
+                daemon_sockfd = setup_client_connection(AUDIO_OUTPUT_REQUEST);
+            }
+            break;
+        case LWS_CALLBACK_RECEIVE:
+            if (daemon_sockfd != -1) {
+                write(daemon_sockfd, in, len);
+            }
+            break;
+        case LWS_CALLBACK_CLOSED:
+            active_ws_connections--;
+            if (active_ws_connections == 0 && daemon_sockfd != -1) {
+                close(daemon_sockfd);
+                daemon_sockfd = -1;
+            }
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+static struct lws_protocols protocols[] = {
+    {
+        "audio-protocol",
+        ws_callback,
+        0,
+        AO_MAX_FRAME_SIZE,
+    },
+    { NULL, NULL, 0, 0 }
+};
+
 int main(int argc, char *argv[]) {
-    int use_stdin = 0;
-    char *audio_file_path = NULL;
-    int record_audio = 0;
-    int output_to_stdout = 0;
-    int request_type;
+    printf("INGENIC WEB AUDIO CLIENT Version: %s\n", VERSION);
 
-    printf("INGENIC AUDIO CLIENT Version: %s\n", VERSION);
+    struct lws_context_creation_info info;
+    memset(&info, 0, sizeof(info));
 
-    if (parse_arguments(argc, argv, &use_stdin, &audio_file_path, &record_audio, &output_to_stdout) != 0) {
-        exit(1);
+    info.port = 8089;
+    info.protocols = protocols;
+
+    struct lws_context *context = lws_create_context(&info);
+    if (!context) {
+        fprintf(stderr, "Failed to create WebSocket context\n");
+        return 1;
     }
 
-    if (record_audio) {
-        request_type = AUDIO_OUTPUT_REQUEST;
+    while (1) {
+        lws_service(context, 1000);
     }
 
-    int control_sockfd = setup_control_client_connection();
-    // Send the request type to the control server
-    write(control_sockfd, &request_type, sizeof(int));
-
-    char control_msg[100];
-    int read_size = read(control_sockfd, control_msg, sizeof(control_msg) - 1);
-    if (read_size > 0) {
-	control_msg[read_size] = '\0';
-//        Don't print the control for now
-//        printf("%s\n", control_msg);
-        if (strcmp(control_msg, "queued") == 0) {
-        printf("There is another client currently playing audio, audio from this client is queued, waiting for current client to finish.\n");
-            close(control_sockfd);
-//            exit(0);  // We can exit the client if it's queued, but not right now
-        }
-    }
-    close(control_sockfd);
-
-    int sockfd = setup_client_connection(AUDIO_OUTPUT_REQUEST);
-
-    if (sockfd < 0) {
-        perror("Failed to connect to daemon");
-        exit(1);
-    }
-
-    printf("[INFO] Connected to daemon\n");
-
-    if (record_audio) {
-        FILE *audio_file = use_stdin ? stdin : fopen(audio_file_path, "rb");
-        if (!audio_file) {
-            perror("fopen");
-            close(sockfd);
-            exit(1);
-        }
-
-        // Send audio output request to the server
-        int request_type = AUDIO_OUTPUT_REQUEST;
-        write(sockfd, &request_type, sizeof(int));
-
-        playback_audio(sockfd, audio_file);
-
-        if (!use_stdin) {
-            fclose(audio_file);
-        }
-    }
-
-    close(sockfd);
+    lws_context_destroy(context);
     return 0;
 }
